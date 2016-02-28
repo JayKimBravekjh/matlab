4f66353e16bf6152fb3b5d964eadad91fdb86559
Jay Kim <jay.kim07@yahoo.com>
2016-02-28 05:11:44 -0800

    myCancelJob.m
    myCancelJob.m

---
+++ myCancelJob.m
@@ -0,0 +1,7 @@
+function myCancelTask(sched, job)
+ array_data = getJobClusterData(clust, job)
+ job_id = array_data(...) % Extract the ID from the data, depending on how
+ % it was stored in the submit function above.
+ command to scheduler canceling job job_id
+ 
+ 
